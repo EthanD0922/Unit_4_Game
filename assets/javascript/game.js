205202724d5b520a5286d1d21f4c5dc309848d19
Ethan <allric0922@gmail.com>
2019-07-21 17:19:48 -0400

    console.logs removed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,15 +7,12 @@ function gameStart(){
     $("#Core").text(crystalTotal)
     coreValue = Math.floor(Math.random() * (120 - 19)) + 19
     $("#powerNeeded").text(coreValue)
-    console.log("Core " + coreValue)
+    
     crystalOneValue = Math.floor(Math.random() * (12 - 1)) + 1
-    console.log("One " + crystalOneValue)
     crystalTwoValue = Math.floor(Math.random() * (12 - 1)) + 1 
-    console.log("Two " + crystalTwoValue)
     crystalThreeValue = Math.floor(Math.random() * (12 - 1)) + 1
-    console.log("Three " + crystalThreeValue)
     crystalFourValue = Math.floor(Math.random() * (12 - 1)) + 1 
-    console.log("Four " + crystalFourValue) 
+    
     $("#wins").text(wins)
     $("#losses").text(losses)
 }
@@ -24,13 +21,11 @@ function winCondition(){
     $("#Core").text(crystalTotal)
     if (crystalTotal === coreValue){
         wins ++
-        console.log("wins " + wins)
         gameStart()
     }
 
     else if (crystalTotal > coreValue){
         losses ++
-        console.log("Losses " + losses)
         gameStart()
     }
 }
@@ -39,23 +34,19 @@ gameStart()
 $("#crystalOne").on("click", function(){
     crystalTotal = crystalTotal + crystalOneValue
     winCondition()
-    console.log("core " + crystalTotal)
 })
 
 $("#crystalTwo").on("click", function(){
     crystalTotal = crystalTotal + crystalTwoValue
     winCondition()
-    console.log("core " + crystalTotal)
 })
 
 $("#crystalThree").on("click", function(){
     crystalTotal = crystalTotal + crystalThreeValue
     winCondition()
-    console.log("core " + crystalTotal)
 })
 
 $("#crystalFour").on("click", function(){
     crystalTotal = crystalTotal + crystalFourValue
     winCondition()
-    console.log("core " + crystalTotal)
 })
